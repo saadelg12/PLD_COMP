52a79d008135e8cef238467e6833ff10c8e9f81a
nihalbgt <nihal.boutadghart@insa.fr>
2025-04-02 08:55:15 +0200

    config

--- compiler/config-IF501.mk
+++ compiler/config-IF501.mk
@@ -1,4 +1,3 @@
-# these values work with the "install-antlr.sh" script provided for the PLD
-ANTLRJAR=../antlr/jar/antlr-4.9.2-complete.jar
-ANTLRINC=../antlr/include
-ANTLRLIB=../antlr/lib/libantlr4-runtime.a
+ANTLRJAR=/home/nihalaa/antlr4-install/antlr-4.13.2-complete.jar
+ANTLRINC=/home/nihalaa/antlr4-install/include/antlr4-runtime/
+ANTLRLIB=/home/nihalaa/antlr4-install/lib/libantlr4-runtime.a
