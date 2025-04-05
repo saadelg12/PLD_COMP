d548e9d90128ba226c9bc1def2daf5654ad9ded4
NkmJunior <junior.noukam@insa-lyon.fr>
2025-04-05 10:28:51 +0200

    config

--- compiler/config-wsl-2025.mk
+++ compiler/config-wsl-2025.mk
@@ -1,4 +1,4 @@
-ANTLRJAR=/home/nihalaa/antlr4-install/antlr-4.13.2-complete.jar
-ANTLRINC=/home/nihalaa/antlr4-install/include/antlr4-runtime/
-ANTLRLIB=/home/nihalaa/antlr4-install/lib/libantlr4-runtime.a
+ANTLRJAR=/home/junior/antlr4-install/antlr-4.13.2-complete.jar
+ANTLRINC=/home/junior/antlr4-install/include/antlr4-runtime/
+ANTLRLIB=/home/junior/antlr4-install/lib/libantlr4-runtime.a
 
